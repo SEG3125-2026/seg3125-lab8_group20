c145b13d5b1cc890744d43e80fde4e2d6547e54b
emilyjette <emily.m.jette@gmail.com>
2026-03-22 22:53:26 -0400

    register.js finished

--- myproject/screens/register.js
+++ myproject/screens/register.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import { globalStyles } from '../styles/global'
 import { Text, View, Button, ScrollView,TextInput} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { Picker } from '@react-native-picker/picker';
 
 
 export default function RegisterScreen(){
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
+    const [classPicked, setClassPicked] = useState('');
 
     const navigation = useNavigation();
         const pressHandler = () => {
@@ -18,6 +20,7 @@ export default function RegisterScreen(){
         <View style={globalStyles.container}>
             <Button title='Home' onPress={pressHandler} />
             <Text style={globalStyles.paragraph}>Register for a Class</Text>
+            <Text style={globalStyles.paragraph}>Note: You can only register for one class at a time.</Text>
                 <TextInput
                 placeholder="Enter your name"
                 value={name}
@@ -30,11 +33,32 @@ export default function RegisterScreen(){
                 onChangeText={setEmail}
                 style={globalStyles.input}
                 />
+
+                <Text style={globalStyles.paragraph}>Select a Class:
+                    <Picker selectedValue={classPicked} onValueChange={(itemValue) => setClassPicked(itemValue)} >
+                            <Picker.Item  value=""  label=""/>
+                            <Picker.Item  value="mondayBallet"  label="Ballet : Monday Night"/>
+                            <Picker.Item  value="wednesdayBallet" label="Ballet: Wednesday Night" />
+                            <Picker.Item  value="fridayBallet" label="Ballet: Friday Afternoon" />
+                            <Picker.Item  value="tuesdayHiphop" label="HipHop : Tuesday Night" />
+                            <Picker.Item  value="thursdayHiphop" label="HipHop: Thursday Night" />
+                            <Picker.Item  value="mondayJazz" label="Jazz: Monday Night" />
+                            <Picker.Item  value="wednesdayJazz" label="Jazz : Wednesday Morning" />
+                            <Picker.Item  value="saturdayJazz" label="Jazz: Saturday Afternoon" />
+                            <Picker.Item  value="tuesdayLyrical" label="Lyrical: Tuesday Night" />
+                            <Picker.Item  value="fridayLyrical" label="Lyrical : Friday Night" />
+                            <Picker.Item  value="sundayLyrical" label="Lyrical: Sunday Morning" />
+                            <Picker.Item  value="wednesdayTap" label="Tap: Wednesday Night" />
+                            <Picker.Item  value="fridayTap" label="Tap : Friday Night" />
+                            <Picker.Item  value="sundayTap" label="Tap: Sunday Afternoon" />
+                    </Picker>
+                </Text>
+
                 <Button title="Submit Registration" onPress={() => {
-                if (!name||!email){
+                if (!name||!email||classPicked===''){
                     alert("Please fill all fields");
                 } else{
-                    alert(`Registered: ${name}, ${email}`);
+                    alert(`${name} has been registered in ${classPicked}`);
                 }
                 }} />
         </View>
